b211d45409ff67d6f56d2f77e389729f4c1a2748
I338157 <a.astono@student.fontys.nl>
2016-05-16 09:21:49 +0200

    May 16th 2016 9:04 AM

--- gp.sql
+++ gp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2016 at 07:26 PM
+-- Generation Time: May 16, 2016 at 09:20 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.21
 
@@ -40,31 +40,11 @@ CREATE TABLE `contacts` (
 --
 
 INSERT INTO `contacts` (`id`, `phoneName`, `phoneNumber`, `timeOfCall`, `notes`, `userID`) VALUES
-(4, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(5, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(6, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(7, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(8, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(9, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(10, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(11, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(12, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(15, 'Aditya', '098237273', '2016-05-03', '                                sjdhsjka', 1),
-(16, 'Aditya', '098237273', '2016-05-03', 'sdasdadsadsadsa', 1),
-(17, 'Aditya', '098237273', '2016-05-03', 'sdasdadsadsadsa', 1),
-(18, 'Aditya', '098237273', '2016-05-03', 'sdasdadsadsadsa', 1),
-(19, 'Aditya', '098237273', '2016-05-03', 'sdasdadsadsadsa', 1),
-(20, 'Aditya', '098237273', '2016-05-03', 'sdasdadsadsadsa', 1),
-(26, 'dfsdfdsfsfsdf', '06862625222', '2016-05-03', 'Call him again next week!', 1),
-(27, 'dfsdfdsfsfsdf', '06862625222', '2016-05-03', 'Call him again next week!', 1),
-(28, 'dfsdfdsfsfsdf', '06862625222', '2016-05-03', 'Call him again next week!', 1),
-(29, 'dfsdfdsfsfsdf', '06862625222', '2016-05-03', 'Call him again next week!', 1),
-(31, 'another', '262482392389', '2016-05-10', 'hfbsdjfjdkfndjsfnds', 2),
-(32, 'Bsbi', 'Kucing', '2016-05-27', 'dhkdhkjdfhksjsdsfdsfdsfsdfs', 1),
-(33, 'Bsbi', 'Kucing', '2016-05-27', 'dhkdhkjdfhksjsdsfdsfdsfsdfs', 1),
-(34, 'Bsbi', 'Kucing', '2016-05-27', 'dhkdhkjdfhksjsdsfdsfdsfsdfs', 1),
-(35, 'Bsbi', 'Kucing', '2016-05-27', 'dhkdhkjdfhksjsdsfdsfdsfsdfs', 1),
-(36, 'AJAX', 'AJAX', '2016-05-02', 'AJAX', 1);
+(43, 'Erik Pieters', '0682627587', '2016-05-01', 'Left Back for Stoke City', 6),
+(44, 'Robin van Persie', '0654253875', '2016-05-03', 'Center Forward for Fenerbache', 6),
+(45, 'Dirk Kuyt', '0678625827', '2016-05-07', 'Center Forward for Feyenoord', 6),
+(46, 'Daley Blind', '0678263752', '2016-05-13', 'Center Back for Manchester United', 6),
+(47, 'Arjen Robben', '0789273826', '2016-05-27', 'Right Winger for Bayern Munich', 6);
 
 -- --------------------------------------------------------
 
@@ -85,8 +65,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`userID`, `username`, `email`, `password`) VALUES
 (1, 'adityaastono', 'aditya.astono@gmail.com', '$2y$10$R7ZFb8eBC8BbNuAzY2moz.b6rE9lnAL0Abct3thVU4Rc/3pBTmuya'),
-(2, 'anotheraditya', 'aditya.astono@hotmail.com', '$2y$10$Rd1pxodqDjHH/xmcfhdk5.AngwtGXPSre2N4l9MVIs/menlqEY.pC'),
-(3, 'adityaastono', 'adjdd@ajskdsdsa.com', '$2y$10$FdSvNn8o4NH.ZTmr7f6mY.YtExEeB0O5xab4ah11CrDOQmJtpuhza');
+(6, 'admin', 'admin@admin.com', '$2y$10$RlDQ3sd/UL4FTBy7MlE0GOLD8tasIEwgVXgxRLwqk0iRRHFZWYz2e');
 
 --
 -- Indexes for dumped tables
@@ -112,12 +91,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
