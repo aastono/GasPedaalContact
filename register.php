b71cb73524b75f143601c3a3aaf2c3af58472964
Aditya Astono <aditya.astono@gmail.com>
2016-05-17 12:47:25 +0200

    Update register.php

--- register.php
+++ register.php
@@ -13,6 +13,7 @@ include 'php/registeruser.php';
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
