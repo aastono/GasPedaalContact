a1fef1494a46791e57d33aea62a3856314b01c73
Aditya Astono <aditya.astono@gmail.com>
2016-05-17 12:46:39 +0200

    Update home.php

--- home.php
+++ home.php
@@ -12,6 +12,7 @@ include 'php/CRUD.php';
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Welcome - <?php echo $userRow['email']; ?></title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
